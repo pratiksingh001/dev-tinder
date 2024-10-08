43047bb4d9f9e4ec9f74f9925da545a973e4654f
pratiksingh001 <singhpratik001@gmail.com>
2024-10-08 13:02:09 +0530

    get API's

--- src/app.js
+++ src/app.js
@@ -18,6 +18,42 @@ app.post("/signup", async (req, res) => {
 b 
     }
 })
+
+// get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email
+
+    try{
+        // * findOne({{email: userEmail}}) method => this will only give single object => this will give the oldest object(not sure)
+        // const users = await User.findOne({email: userEmail})
+        // res.send(users)
+        // * find({{email: userEmail}}) method =>  will give all objects with same email
+        const users = await User.find({email: userEmail})
+        if(users.length === 0){
+            res.status(404).send("User not found")
+        }else{
+            res.send(users)
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// Feed API - /feed -> this Api will get all the users form DB
+app.get('/feed', async (req, res) => {
+    try{
+        // by passing empty object in the find function -> we will get the entire list
+        const users = await User.find({})
+        if(users.length === 0){
+            res.status(400).send("No user found")
+        }else{
+            res.send(users)
+        }
+    }catch(err){
+        res.status(400).send('Something went wrong')
+    }
+})
+
 connectDB().then(() => {
     console.log("DB connected successfully")
     app.listen(3000, () => {
